ca771308cf2a899edfe8e79e288e5dd8341d3ee9
olehabib <olehabib98@gmail.com>
2019-07-06 00:48:07 +0100

    mage

--- frontend/mage/controller/LoginAdminRoute.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "LoginAdmin.php";
-
-if(isset($_POST['login_admin']))
-	new LoginAdmin;
-else
-	die("Something wrong, my friend!!");
--- frontend/mage/controller/LoginRoute.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "Login.php";
-
-if(isset($_POST['login_user']))
-	new Login;
-else
-	die("Something wrong, my friend!!");
